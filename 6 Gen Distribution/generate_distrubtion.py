e8c6ff4ea74fdb0580f3857c93e9a5fff80d28bf
u7145232 <u7145232@anu.edu.au>
2024-05-27 16:14:37 +1000

    update framework

--- 6 Gen Distribution/generate_distrubtion.py
+++ 6 Gen Distribution/generate_distrubtion.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 list_of_terms = ['plains', 'hilly', 'mountainous', 'islands']
-params = ['frequency', 'lacunarity', 'octaves']
+valuenoise_params = ['frequency', 'lacunarity', 'octaves']
+distribution_params = ['num_modes', 'centre', 'spread', 'min_value', 'max_value', 'skew', 'kurtosis']
 
+#todo:
+#come up with some more terrain descriptors you think might fit unique types of distributions
+#match the descriptors to a range of distrubution and value noise params
+#check the appearance of the terrain generated to see if it matches/ is different enough
+
+#depending on the input descriptor, create a distribtuion (28 numbers) using a random value in the range of each of the 
+#param ranges
 
