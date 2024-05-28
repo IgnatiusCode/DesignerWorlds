4e40a26f264521dff88cac0e5963c95d59ae26b2
wonster <wooonster@outlook.com>
2024-05-28 00:01:52 -0700

    change with simplex noise

--- 3 Generate/valuenoise.cpp
+++ 3 Generate/valuenoise.cpp
@@ -1,67 +1,84 @@
 /// \file valuenoise.cpp
 /// \brief Code for the designer world class CDesignerWorld.
 
-//Copyright Ian Parberry, November 2013.
+// Copyright Ian Parberry, November 2013.
 //
-//This file is made available under the GNU All-Permissive License.
+// This file is made available under the GNU All-Permissive License.
 //
-//Copying and distribution of this file, with or without
-//modification, are permitted in any medium without royalty
-//provided the copyright notice and this notice are preserved.
-//This file is offered as-is, without any warranty.
+// Copying and distribution of this file, with or without
+// modification, are permitted in any medium without royalty
+// provided the copyright notice and this notice are preserved.
+// This file is offered as-is, without any warranty.
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <limits>
-
 #include <algorithm>
+#include <math.h>
 
 #include "valuenoise.h"
+#include "SimplexNoise.cpp"
+SimplexNoise simplexNoise;
+
+#include "PerlinNoise.hpp"
+
+const siv::PerlinNoise::seed_type seed = 123456u;
+const siv::PerlinNoise perlin{seed};
+
+// const int CELLSIZE = 4000; // width of square grid
+// const int NUMOCTAVES = 8;  // number of octaves of 1/f noise
+// const int ALTITUDE = 4000; // altitude scale value
+
+// SimplexNoise simplexNoise;
 
 /// Initialize the permutation table.
 
-void CDesignerWorld::Initialize(){
-  //start with identity permutation
-  for(int i=0; i<SIZE; i++)
+void CDesignerWorld::Initialize()
+{
+  // start with identity permutation
+  for (int i = 0; i < SIZE; i++)
     m_nPermute[i] = i;
 
-  //randomize 
-  for(int i=SIZE-1; i>0; i--){
-    int j = rand() % (i + 1); //note the fix to Perlin's original code
+  // randomize
+  for (int i = SIZE - 1; i > 0; i--)
+  {
+    int j = rand() % (i + 1); // note the fix to Perlin's original code
     int temp = m_nPermute[i];
     m_nPermute[i] = m_nPermute[j];
     m_nPermute[j] = temp;
-  } //for
-} //Initialize
+  } // for
+} // Initialize
 
 /// Set the value table.
 /// \param table Table of values.
 /// \param n Size of table.
 
-void CDesignerWorld::SetValueTable(int table[], const int n){
-  //check that the values add up to 256
+void CDesignerWorld::SetValueTable(int table[], const int n)
+{
+  // check that the values add up to 256
   int sum = 0;
-  for(int i=0; i<n; i++)
+  for (int i = 0; i < n; i++)
     sum += table[i];
-  if(sum != SIZE){
+  if (sum != SIZE)
+  {
     printf("Height distribution values must sum to %d, not %d\n", SIZE, sum);
-    return; //fail and bail
-  } //if
+    return; // fail and bail
+  } // if
 
-  //fill the table
-  float delta = 2.0f/(float)(n-1); //interval size
-  float min = -1.0f; //lower limit if interval
-  int k = 0; //fill in m_fPosition[k]
-  for(int i=0; i<n-1; i++){
-    for(int j=0; j<table[i]; j++)
-      m_fPosition[k++] = min + delta * (float)rand()/(float)RAND_MAX;
+  // fill the table
+  float delta = 2.0f / (float)(n - 1); // interval size
+  float min = -1.0f;                   // lower limit if interval
+  int k = 0;                           // fill in m_fPosition[k]
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = 0; j < table[i]; j++)
+      m_fPosition[k++] = min + delta * (float)rand() / (float)RAND_MAX;
     min += delta;
-  } //for
+  } // for
 
-  //missed the largest values, get them now
-  for(int j=0; j<table[n-1]; j++)
+  // missed the largest values, get them now
+  for (int j = 0; j < table[n - 1]; j++)
     m_fPosition[k++] = 1.0f;
-} //SetValueTable
+} // SetValueTable
 
 /// Get random height value at a point in the terrain.
 /// Computes 1/f noise using the sample table.
@@ -72,19 +89,37 @@ void CDesignerWorld::SetValueTable(int table[], const int n){
 /// \param n Number of octaves
 /// \return Height value between 0.0 and 1.0
 
-float CDesignerWorld::GetHeight(float x, float z, float a, float b, int n){
-  float result = 0.0f; //resulting height
-  float scale = 1.0f; //scale of next octave
+float CDesignerWorld::GetHeight(float x, float z, float a, float b, int n)
+{
+  float result = 0.0f; // resulting height
+  float scale = 1.0f;  // scale of next octave
+  float sum_scales = 1.0f;
+  float decrese_unit = scale / n;
 
+  for (int i = 0; i < n; i++)
+  // while(n > 0)
+  { // for each octave
+    result += scale * simplexNoise.noise(x, z);
+    scale *= a; // scale down amplitude of next octave
+    sum_scales += scale;
+    x *= b;
+    z *= b; // scale down wavelength of next octave
+  } // for
+
+  result = pow(result / sum_scales, 1.9f);
+  return (1.0f + result * 1.414213f * (a - 1.0f) / (scale - 1.0f)) * 2.0f;
   
-  for(int i=0; i<n; i++){ //for each octave 
-    result += scale * noise(x, z);
-    scale *= a; //scale down amplitude of next octave
-    x *= b; z *= b; //scale down wavelength of next octave
-  } //for
+  // return pow(result, 1.95f) * 2;
+  // printf("%f      ",a);
+  // printf("%f",result);
+  // float normalizedResult = (result * 1.414213f * (a - 1.0f) / (scale - 1.0f));
+  // // float height = min_height + normalizedResult * (max_height - min_height);
+  // float height = (1.0f + result * 1.414213f * (a - 1.0f) / (scale - 1.0f)) * 2.0f;
+  // // printf("%f", normalizedResult);
 
-  return (1.0f + result * 1.414213f * (a - 1.0f)/(scale - 1.0f))/2.0f; //scale to [0.0, 1.0]
-} //GetHeight
+  // return height;
+  // return height / ALTITUDE; // scale to [0.0, 1.0]
+} // GetHeight
 
 /// Get random height value for a single octave at a point in the terrain.
 /// This is Value Noise. If you understand Perlin Noise, then this code will
@@ -93,8 +128,8 @@ float CDesignerWorld::GetHeight(float x, float z, float a, float b, int n){
 /// \param x X coordinate of point at which to sample
 /// \param z Z coordinate of point at which to sample
 /// \return Height value between -1.0 and 1.0
-
-float CDesignerWorld::noise(float x, float z){
+float CDesignerWorld::noise(float x, float z)
+{
   const int dx = (int)x;
   const float sx = s_curve(x - dx);
   const int i = m_nPermute[MASK & dx];
@@ -111,4 +146,5 @@ float CDesignerWorld::noise(float x, float z){
   const float v1 = lerp(sx, u0, u1);
 
   return lerp(sz, v0, v1);
-} //noise
+
+} // noise
