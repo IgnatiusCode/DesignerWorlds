ef2e027c9a3dc59f87db3c2e930337f928baa71f
u7145232 <u7145232@anu.edu.au>
2024-05-29 15:12:00 +1000

    refactor sim erosion

--- 5 Erosion Sim/simulate_erosion.py
+++ 5 Erosion Sim/simulate_erosion.py
@@ -18,19 +18,25 @@ nrows, ncols = elevation.shape
 valley = True
 plateau = False
 plateau_height = 10000
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
 
+def initialise_model(nrows,ncols, elevation, cellsize):
+    mg = RasterModelGrid((nrows, ncols), cellsize)
+    z = mg.add_field('topographic__elevation', elevation.flatten(), at='node')
+    #plt.plot(mg.x_of_node, mg.y_of_node, '.')
+    imshow_grid(mg, 'topographic__elevation')
+    axes[0].set_title('Initial Elevation')
+    plt.show()
+    
+    return z,mg
 
-mg = RasterModelGrid((nrows, ncols), cellsize)
-z = mg.add_field('topographic__elevation', elevation.flatten(), at='node')
-#plt.plot(mg.x_of_node, mg.y_of_node, '.')
-D = 7 # m2/yr transport coefficient
-dt = 0.3 * mg.dx * mg.dx / D
-mg.set_closed_boundaries_at_grid_edges(True, False, True, False)
-qs = mg.add_zeros('sediment_flux', at='link')
+def get_valley(z,mg,grid_width,valley_width):
 
-if valley:
-    fault_trace_y = 50.0 + 0.25 * mg.x_of_node
-    fault_trace_y1 = 250.0 + 0.25 * mg.x_of_node
+    valley_min = np.random.uniform(0, grid_width - valley_width)
+    valley_max = valley_min+valley_width
+
+    fault_trace_y = valley_min + 0.25 * mg.x_of_node
+    fault_trace_y1 = valley_max + 0.25 * mg.x_of_node
 
     condition = (mg.y_of_node > fault_trace_y) & (mg.y_of_node < fault_trace_y1)
     #z[condition] -= 1000
@@ -41,41 +47,59 @@ if valley:
 
     normalized_distance = distance_from_center / (valley_width / 2.0)
     z[condition] = max_depth * (normalized_distance[condition]-1)
+    
         
-z[mg.core_nodes]+10000
+#z[mg.core_nodes]+10000
 
-if plateau:
+def get_plateaus(z,mg, plateau_height):
     z[mg.core_nodes>plateau_height] = plateau_height
 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
+def get_erosion(z,mg,D=7,dt_coeff=0.3):
+    D = 7 # m2/yr transport coefficient
+    dt = dt_coeff * mg.dx * mg.dx / D
+    mg.set_closed_boundaries_at_grid_edges(True, False, True, False)
+    qs = mg.add_zeros('sediment_flux', at='link') 
+    for i in range(10):
+        print('step '+ str(i))
+        g = mg.calc_grad_at_link(z)
+        qs[mg.active_links] = -D * g[mg.active_links]
+        dzdt = -mg.calc_flux_div_at_node(qs)
+        print(dzdt[mg.core_nodes] * dt)
+        z[mg.core_nodes] += dzdt[mg.core_nodes] * dt
+    
+    
+    imshow_grid(mg, 'topographic__elevation')
+    axes[1].set_title('Final Elevation After Erosion')
+    plt.tight_layout()
+    plt.show()
 
-# Plot the initial elevation grid in the first subplot
-imshow_grid(mg, 'topographic__elevation')
-axes[0].set_title('Initial Elevation')
-#imshow_grid(mg, 'topographic__elevation')
-plt.show()
-for i in range(10):
-    print('step '+ str(i))
-    g = mg.calc_grad_at_link(z)
-    qs[mg.active_links] = -D * g[mg.active_links]
-    dzdt = -mg.calc_flux_div_at_node(qs)
-    print(dzdt[mg.core_nodes] * dt)
-    z[mg.core_nodes] += dzdt[mg.core_nodes] * dt
-
-imshow_grid(mg, 'topographic__elevation')
-axes[1].set_title('Final Elevation After Erosion')
-
-# Display the plots
-plt.tight_layout()
-plt.show()
-
-#imshow_grid(mg, 'topographic__elevation')
-#splt.show()
-
-elevation_grid = mg.node_vector_to_raster(z)
-dem_output_file = "elevation_dem.asc"
+def save_to_ascii(mg,z,dem_output_file ="elevation_dem.asc"):
+    elevation_grid = mg.node_vector_to_raster(z)
+    dem_output_file = "elevation_dem.asc"
 
 # Save the DEM
-load_dem.save_dem_as_ascii(elevation_grid, mg.dx, dem_output_file)
+    load_dem.save_dem_as_ascii(elevation_grid, mg.dx, dem_output_file)
 
-print(f"DEM file saved as {dem_output_file}")
+    print(f"DEM file saved as {dem_output_file}")
+
+
+params = [[True, nrows, 200],[True,7,0.3],[False],[False]]
+#main
+def erode_terrain(params, nrows,ncols,elevation,cellsize):
+
+    z, mg = initialise_model(nrows,ncols,elevation,cellsize)
+    if params[0][0] == True:
+        get_valley(z,mg,params[0][1], params[0][2])
+    if params[1][0] == True:
+        get_erosion(z,mg,params[1][1], params[1][2])
+    if params[3][0] == True:
+        get_plateaus(z,mg,params[3][1])
+    
+    save_to_ascii(mg,z)
+
+erode_terrain(params,nrows,ncols,elevation,cellsize)
+        
+        
+
+
+
